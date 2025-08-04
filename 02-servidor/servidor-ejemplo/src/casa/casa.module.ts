f626d0f1c5902000fd07d93e9ffdf1f09575aab5
MYBTIC <maxipillo2@gmail.com>
2025-08-04 16:35:30 -0500

    Commit Final

---
+++ 02-servidor/servidor-ejemplo/src/casa/casa.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { casaProviders } from './casa.repository';
+import { CasaController } from './casa.controller';
+import { DatabaseModule } from '../database/database.module'; // Asegúrate de que la ruta sea correcta
+import { CasaService } from './casa.service';
+
+
+@Module({
+    imports: [
+        DatabaseModule
+    ],
+    controllers: [CasaController],
+    providers: [
+        ...casaProviders,
+        CasaService,
+    ],
+    exports: [
+        ...casaProviders,
+        CasaService,
+    ]
+})
+export class CasaModule { }
